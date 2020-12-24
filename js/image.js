f33687ed09f45449b94c8e02cf8e7858b94f8142
Dustbin <dustbin@localhost.localdomain>
2020-12-24 01:12:16 -0600

    starting the build of the weighting map

--- js/image.js
+++ js/image.js
@@ -1,31 +1,102 @@
-function Image(texture){
-	this.texture = texture;
-	this.material = new THREE.MeshBasicMaterial({map: this.texture});
-	let geometry = new THREE.BufferGeometry();
-	let width = this.texture.image.width;
-	let height = this.texture.image.height;
-	let vertices = new Float32Array( [
-		0,0,0,
-		width,0,0,
-		width,height,0,
+class Image {
+	constructor(texture){
+		this.texture = texture;
+		this.material = new THREE.MeshBasicMaterial({map: this.texture});
+		this.mesh = new THREE.Mesh( this.buildGeometry(), this.material );
+	}
+	width(){
+		if(this.texture.image){
+			return this.texture.image.width;
+		}else{
+			return 0;
+		}
+	}
+	height(){
+		if(this.texture.image){
+			return this.texture.image.height;
+		}else{
+			return 0;
+		}
+	}
+	translateX(x){
+		if(this.mesh){
+			this.mesh.translateX(x);
+		}
+	}
+	translateY(y){
+		if(this.mesh){
+			this.mesh.translateY(y);
+		}
+	}
+	translateZ(z){
+		if(this.mesh){
+			this.mesh.translateZ(z);
+		}
+	}
+	buildGeometry(){
+		let geometry = new THREE.BufferGeometry();
+		let vertices = new Float32Array( [
+			0,0,0,
+			this.width(),0,0,
+			this.width(),this.height(),0,
 
-		0,0,0,
-		width,height,0,
-		0,height,0
-	] );
-	let uv = new Float32Array( [
-		0,0,
-		1,0,
-		1,1,
-		
-		0,0,
-		1,1,
-		0,1
-	] );
-	geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
-	geometry.setAttribute( 'uv', new THREE.BufferAttribute( uv, 2 ) );
-	this.mesh = new THREE.Mesh( geometry, this.material );
-	this.mesh.translateX(-width);
-	this.mesh.translateY(height/-2);
-	this.mesh.translateZ(-1);
+			0,0,0,
+			this.width(),this.height(),0,
+			0,this.height(),0
+		] );
+		let uv = new Float32Array( [
+			0,0,
+			1,0,
+			1,1,
+
+			0,0,
+			1,1,
+			0,1
+		] );
+		geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
+		geometry.setAttribute( 'uv', new THREE.BufferAttribute( uv, 2 ) );
+		return geometry;
+	}
+	buildWeights(renderer){
+		let camera = new THREE.OrthographicCamera( 0, this.width(), this.height(), 0, -1, 1 );
+		let renderTarget = new THREE.WebGLRenderTarget(
+			this.width(),
+			this.height(),
+			{
+				format: THREE.RGBFormat,
+				depthBuffer: false
+			}
+		)
+		let material = new THREE.ShaderMaterial( {
+			uniforms: { map: { type: 't', value: this.texture } },
+			vertexShader: Image.weightVS,
+			fragmentShader: Image.weightFS
+		} );
+		let mesh = new THREE.Mesh( this.buildGeometry(), material );
+		let scene = new THREE.Scene();
+		scene.add(mesh);
+		renderer.setRenderTarget(renderTarget);
+		renderer.render(scene,camera);
+		renderer.setRenderTarget(null);
+		this.textureWeights = renderTarget.texture;
+		this.materialWeights = new THREE.MeshBasicMaterial( {map: this.textureWeights} );
+		this.meshWeights = new THREE.Mesh( this.buildGeometry(), this.materialWeights );
+	}
+	static weightVS = `
+		varying vec2 vUv;
+
+		void main()	{
+			vUv = uv;
+
+			gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+		}
+	`;
+	static weightFS = `
+		uniform sampler2D map;
+		varying vec2 vUv;
+
+		void main()	{
+			gl_FragColor = texture2D(map, vUv);
+		}
+	`;
 }
--- js/main.js
+++ js/main.js
@@ -16,16 +16,21 @@ window.onload = function(){
 
 	scene = new THREE.Scene();
 	scene.background = new THREE.Color( 0xffffff );
+
 	new THREE.TextureLoader().load(
 		"test.jpg",
 		function(texture){
 			image = new Image(texture);
+			image.translateX(image.width()*-1);
+			image.translateY(image.height()/-2);
+			image.translateZ(-1);
 			scene.add(image.mesh);
+			image.buildWeights(renderer);
 		}
 	);
 
 	triangles.push(new Triangle([0,0,100,0,100,100],1,0xffff00,0.5));
-	triangles.push(new Triangle([100,0,200,0,200,100],1,0xffff00,1));
+	triangles.push(new Triangle([50,0,150,0,150,100],2,0xffff00,0.3));
 	scene.add( triangles[0].mesh );
 	scene.add( triangles[1].mesh );
 
