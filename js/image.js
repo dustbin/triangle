4244c6908be08cd66e1e50e4e21c33e3786afa09
Dustbin <dustbin@localhost.localdomain>
2020-12-28 22:29:57 -0600

    faster compare

--- js/image.js
+++ js/image.js
@@ -36,13 +36,17 @@ class Image extends THREE.Object3D {
 			vertexShader: Image.VShader,
 			fragmentShader: Image.FShaderCompare
 		} );
-		let mesh = new THREE.Mesh( Image.buildGeometry(1,1), material );
+		let mesh = new THREE.Mesh( Image.buildGeometry(this.width,1), material );
 		let scene = new THREE.Scene();
 		scene.add(mesh);
-		let renderTarget = Image.createRender(renderer,1,1,scene);
-		let result = new Uint8Array(4);
-		renderer.readRenderTargetPixels(renderTarget,0,0,1,1,result);
-		return result[0]*0x1000000+result[1]*0x10000+result[2]*0x100+result[3];
+		let renderTarget = Image.createRender(renderer,this.width,1,scene);
+		let result = new Uint8Array(this.width*4);
+		renderer.readRenderTargetPixels(renderTarget,0,0,this.width,1,result);
+		let ret = 0;
+		for(let i=0;i<this.width;++i){
+			ret += result[i*4]*0x1000000+result[i*4+1]*0x10000+result[i*4+2]*0x100+result[i*4+3];
+		}
+		return ret;
 	}
 	static createTexture(renderer,width,height,scene){
 		return Image.createRender(renderer,width,height,scene).texture;
@@ -108,16 +112,12 @@ class Image extends THREE.Object3D {
 			vec4 temp;
 			vec2 coord;
 
-			float i = xpix*0.5, j;
+			float i = ypix*0.5;
 			while(i < 1.0){
-				j = ypix*0.5;
-				while(j < 1.0){
-					coord = vec2(i,j);
-					temp = abs( texture2D(map2,coord) - texture2D(map1,coord) ) * (texture2D(mapw,coord)*0.5+0.5);
-					diff += int( floor( (temp.x+temp.y+temp.z)*1000.0/3.0 ) );
-					j += ypix;
-				}
-				i += xpix;
+				coord = vec2(vUv.x,i);
+				temp = abs( texture2D(map2,coord) - texture2D(map1,coord) ) * (texture2D(mapw,coord)*0.5+0.5);
+				diff += int( floor( (temp.r+temp.g+temp.b)*1000.0/3.0 ) );
+				i += ypix;
 			}
 			gl_FragColor = vec4(
 				float((diff/(256*256*256))%256)/255.0,
